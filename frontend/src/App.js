c5be6881440c280ecddc8bd27670a91929da3e30
Varun <varun898080@gmail.com>
2023-04-29 18:53:37 +0530

    facerec

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
-import react from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from './Components/Home'
-import Navbar from "./Components/Navbar"
-import FaceRecognition from "./Components/faceRecognition"
-import "./App.css"
+import react from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import FaceRecognition from "./Components/faceRecognition";
+// import "./Home.css";
 function App() {
   return (
     <div>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/face' exact element={<FaceRecognition />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/face" exact element={<FaceRecognition />} />
         </Routes>
       </Router>
     </div>
-
   );
 }
 
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,22 +1,21 @@
-import react from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import react from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
-  const location = useLocation()
+  const location = useLocation();
   return (
-      <nav className="navbar">
-        <Link to='/'>
-          <li className={location.pathname === '/' ? 'nav-bar-active' : ''}>
-            Home
-      </li>
-        </Link>
+    <nav className="navbar">
+      <Link to="/">
+        <li className={location.pathname === "/" ? "nav-bar-active" : ""}>
+          Home
+        </li>
+      </Link>
 
-        <Link to='/face'>
-          <li className={location.pathname === '/face' ? 'nav-bar-active' : ''}>
-            FaceRecogn
-      </li>
-        </Link>
-
-      </nav>
-  )
+      <Link to="/face">
+        <li className={location.pathname === "/face" ? "nav-bar-active" : ""}>
+          FaceRecognition
+        </li>
+      </Link>
+    </nav>
+  );
 }
